3b22cf6e5e0e5c1815dfb56b79f23a383d2ab277
maxdanko2000 <maxdanko12345@yandex.ru>
2021-10-04 23:47:57 +0300

    feat: message

--- js/burger.js
+++ js/burger.js
@@ -1,6 +1,7 @@
 let button = document.querySelector('.menu__btn');
 
-button.addEventListener('click', e =>{
+console.log("Уважаемые разработчики! прошу проверить мою работу в последний день, так как я вношу ещё доработки по 420px. Надеюсь на фидбэк)");
+button.addEventListener('click', e => {
     let burger = document.querySelector('.welcome');
 
     burger.classList.toggle('close');
