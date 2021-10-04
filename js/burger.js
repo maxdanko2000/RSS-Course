33933981b7ce0bc0ab0b1ffa6f89a5d4bcda21b3
maxdanko2000 <maxdanko12345@yandex.ru>
2021-10-04 23:48:35 +0300

    feat: mesaage

--- js/burger.js
+++ js/burger.js
@@ -1,6 +1,6 @@
 let button = document.querySelector('.menu__btn');
 
-console.log("Уважаемые разработчики! прошу проверить мою работу в последний день, так как я вношу ещё доработки по 420px. Надеюсь на фидбэк)");
+alert("Уважаемые разработчики! прошу проверить мою работу в последний день, так как я вношу ещё доработки по 420px. Надеюсь на фидбэк)");
 button.addEventListener('click', e => {
     let burger = document.querySelector('.welcome');
 
