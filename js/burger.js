b086d61363c74c4b600d84bf11093d3877b1b7c0
maxdanko2000 <maxdanko12345@yandex.ru>
2021-10-06 15:14:42 +0300

    feat: add media3

--- js/burger.js
+++ js/burger.js
@@ -1,6 +1,6 @@
 let button = document.querySelector('.menu__btn');
 
-alert("Уважаемые разработчики! прошу проверить мою работу в последний день, так как я вношу ещё доработки по 420px. Надеюсь на фидбэк)");
+
 button.addEventListener('click', e => {
     let burger = document.querySelector('.welcome');
 
