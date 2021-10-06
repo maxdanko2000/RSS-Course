07933aa6950cda3dd7a9b75ef2d057ad42256a41
maxdanko2000 <maxdanko12345@yandex.ru>
2021-10-06 20:39:05 +0300

    feat: fix alert

--- js/burger.js
+++ js/burger.js
@@ -1,6 +1,6 @@
 let button = document.querySelector('.menu__btn');
 
-alert('Совсем забыл про бургер форму) Завтра всё будет')
+alert('Совсем забыл про бургер форму) Завтра всё будет, оставьте контакты для связи')
 button.addEventListener('click', e => {
     let burger = document.querySelector('.welcome');
 
