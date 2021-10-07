30cadd68c0a97f3b08d290ee43c872d77fc6d51e
maxdanko2000 <maxdanko12345@yandex.ru>
2021-10-07 18:41:26 +0300

    feat: add some fix on burger and form

--- js/burger.js
+++ js/burger.js
@@ -1,6 +1,5 @@
 let button = document.querySelector('.menu__btn');
 
-alert('Совсем забыл про бургер форму) Завтра всё будет, оставьте контакты для связи')
 button.addEventListener('click', e => {
     let burger = document.querySelector('.welcome');
 
