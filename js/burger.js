e727dd902f879ebbd833421b8bb2cab88a8bed26
maxdanko2000 <maxdanko12345@yandex.ru>
2021-10-11 18:45:48 +0300

    feat: add alert message

--- js/burger.js
+++ js/burger.js
@@ -1,5 +1,7 @@
 let button = document.querySelector('.menu__btn');
 
+
+alert('Уважаемые проверяющие. Просьба проверить таск в последний день кроссчека, т.к. ещё ведутся доработки. Надеюсь на фидбэк')
 button.addEventListener('click', e => {
     let burger = document.querySelector('.welcome');
 
