fc9988df23843f18843eb8aebac90cb963165b21
maxdanko2000 <maxdanko12345@yandex.ru>
2021-10-25 22:03:42 +0300

    feat: add time/date showing [25.10.2021]

--- js/index.js
+++ js/index.js
@@ -1,1 +1,21 @@
 alert('Уважаемые проверяющие! Таск ещё не доделан, прошу проверить в последний день дедлайна. Надеюсь на фидбэк.')
+
+const time = document.querySelector('.time');
+const date = document.querySelector('.date');
+
+function showTime() {
+    const date = new Date();
+    const currentTime = date.toLocaleTimeString();
+    time.textContent = currentTime;
+    setTimeout(showTime, 1000);
+}
+showTime();
+
+function showDate() {
+    const dateText = new Date();
+    const currentDate = dateText.toLocaleDateString();
+    date.textContent = currentDate;
+    setTimeout(showDate, 1000);
+}
+showDate();
+
