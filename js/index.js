be71240441bce7b074bf452e4a1170141bba7724
maxdanko2000 <maxdanko12345@yandex.ru>
2021-10-25 22:57:28 +0300

    feat: add function timeOfDay[25.10.2021 22:57]

--- js/index.js
+++ js/index.js
@@ -2,11 +2,13 @@ alert('Уважаемые проверяющие! Таск ещё не доде�
 
 const time = document.querySelector('.time');
 const date = document.querySelector('.date');
+const greeting = document.querySelector('.greeting');
 
 function showTime() {
     const date = new Date();
     const currentTime = date.toLocaleTimeString();
     time.textContent = currentTime;
+    showDate();
     setTimeout(showTime, 1000);
 }
 showTime();
@@ -15,7 +17,29 @@ function showDate() {
     const dateText = new Date();
     const currentDate = dateText.toLocaleDateString();
     date.textContent = currentDate;
-    setTimeout(showDate, 1000);
 }
-showDate();
 
+function showGreeting() {
+    const date = new Date();
+    const hours = date.getHours();
+    function getTimeOfDay() {
+        let timeDay;
+        if (hours >= 6 && hours <= 11) {
+            timeDay = 'morning';
+            return timeDay;
+        } else if (hours >= 12 && hours <= 17) {
+            timeDay = 'afternoon'
+            return timeDay;
+        } else if (hours >= 18 && hours <= 23) {
+            timeDay = 'evening'
+            return timeDay;
+        } else if (hours >= 0 && hours <= 5) {
+            timeDay = 'night'
+            return timeDay;
+        }
+    }
+    const timeOfDay = getTimeOfDay();
+    const greetingText = `Good ${timeOfDay}`;
+    greeting.textContent = greetingText;
+}
+showGreeting();
