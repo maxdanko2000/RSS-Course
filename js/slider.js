9cbba4e7b8a38c698317ef44ac4431d9d91e3fc1
maxdanko2000 <maxdanko12345@yandex.ru>
2021-10-26 00:00:23 +0300

    feat: correcting code in js file[26.10.2021 00:00]

--- js/slider.js
+++ js/slider.js
@@ -15,13 +15,13 @@ function setBg() {
     //morning
     if (hours >= 6 && hours <= 11) {
         body.style.backgroundImage = `url(./assets/img/images/morning/${bgNum}.jpg)`;
-
         //afternoon
     } else if (hours >= 12 && hours <= 17) {
         body.style.backgroundImage = `url(./assets/img/images/day/${bgNum}.jpg)`;
         //evening
     } else if (hours >= 18 && hours <= 23) {
-        body.style.backgroundImage = `url(./assets/img/images/evening/${bgNum}.jpg)`;        //night
+        body.style.backgroundImage = `url(./assets/img/images/evening/${bgNum}.jpg)`;
+        //night    
     } else if (hours >= 0 && hours <= 5) {
         body.style.backgroundImage = `url(./assets/img/images/night/${bgNum}.jpg)`;
     }
