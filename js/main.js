bdde17a5a8b2cee39c3fe9d33ba12cb1ab3c146a
maxdanko2000 <maxdanko12345@yandex.ru>
2021-11-21 17:29:15 +0300

    refactor: add new images

---
+++ js/main.js
@@ -0,0 +1,13 @@
+const artistsSection = document.querySelector('.actists-section');
+const picturesSection = document.querySelector('.pictures-section');
+
+function addStyle() {
+
+}
+
+function removeStyle() {
+
+}
+
+picturesSection.addEventListener('click', addStyle)
+artistsSection.addEventListener('click', removeStyle)
