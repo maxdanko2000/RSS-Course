fb38836a274e72e9269174f245f00afde1c4ee14
maxdanko2000 <maxdanko12345@yandex.ru>
2021-11-22 18:18:34 +0300

    feat: install webpack and add fucn to settings

--- js/main.js
+++ js/main.js
@@ -1,11 +1,19 @@
 const artistsSection = document.querySelector('.actists-section');
 const picturesSection = document.querySelector('.pictures-section');
+
 const settingSection = document.querySelector('.settings-section')
+
 const mainContent = document.querySelector('.main-section')
+
 const btnSettings = document.querySelector('.buttons-settings')
 const btnSave = document.querySelector('.buttons-save')
+const btnHome = document.querySelector('.btn-home')
+
+
+const categorySection = document.querySelector('.categories-section')
 const categoryContent = document.querySelector('.categories-content');
 const categoryItem = document.querySelector('.category-item');
+
 
 function spawnCategories() {
     for (let i = 0; i < 12; i++) {
@@ -16,23 +24,34 @@ function spawnCategories() {
     }
 }
 
-spawnCategories();
+function showArtistCat() {
+    spawnCategories();
+    hideMain();
+    categorySection.style.display = 'block';
+}
+
+function showPictureCat() {
+
+}
 
 function hideMain() {
     mainContent.style.display = 'none';
     btnSettings.style.display = 'none'
-    btnSave.style.display = 'block'
+    console.log('hided');
 }
 
 function showMain() {
     mainContent.style.display = '';
     btnSettings.style.display = ''
     btnSave.style.display = 'none'
+    categorySection.style.display = 'none'
+    window.location.reload();
 }
 
 function showSettings() {
     hideMain();
     settingSection.style.display = 'flex';
+    btnSave.style.display = 'block'
 }
 
 function hideSettings() {
@@ -40,22 +59,10 @@ function hideSettings() {
     showMain();
 }
 
-function addStyle() {
-
-}
-
-function removeStyle() {
-
-}
-
-function showPictureCat() {
-
-}
-
-function showArtistCat() {
-
-}
+btnHome.addEventListener('click', showMain)
 btnSave.addEventListener('click', hideSettings)
 btnSettings.addEventListener('click', showSettings)
 picturesSection.addEventListener('click', showPictureCat)
 artistsSection.addEventListener('click', showArtistCat)
+
+
